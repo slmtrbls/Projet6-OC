ecc73899111b8747ff7211d0fb3e6c26f4c6ba5e
slmtrbls <slmtrbls@gmail.com>
2022-07-20 18:26:54 +0200

    last commit

--- models/Sauce.js
+++ models/Sauce.js
@@ -10,8 +10,8 @@ const sauceSchema = mongoose.Schema({
   heat: { type: Number, min: 1, max: 10 },
   likes: { type: Number },
   dislikes: { type: Number },
-  usersLiked: [String],
-  usersDisliked: [String]
+  usersLiked: [],
+  usersDisliked: []
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
